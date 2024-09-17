ec86a6347ab377e24f84e0854b9586d014a631f7
crthpl <theo@reinsberg.info>
2024-09-16 22:02:53 -0700

    fix everything

--- src/test/java/net/vulkanmod/ComputeTest.java
+++ src/test/java/net/vulkanmod/ComputeTest.java
@@ -6,7 +6,6 @@ import net.vulkanmod.vulkan.Vulkan;
 import net.vulkanmod.vulkan.memory.StorageBuffer;
 import net.vulkanmod.vulkan.shader.ComputePipeline;
 import net.vulkanmod.vulkan.shader.GraphicsPipeline;
-import net.vulkanmod.vulkan.shader.descriptor.Descriptor;
 import net.vulkanmod.vulkan.shader.descriptor.BufferDescriptor;
 import net.vulkanmod.vulkan.shader.layout.AlignedStruct;
 import org.junit.jupiter.api.Test;
@@ -14,14 +13,12 @@ import org.lwjgl.system.MemoryUtil;
 
 import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.lwjgl.vulkan.VK10.VK_DESCRIPTOR_TYPE_STORAGE_BUFFER_DYNAMIC;
 
 public class ComputeTest {
     @Test
-    public static void main(String[] args) throws Exception {
+    public void main() {
         Platform.init();
         Vulkan.initHeadless();
 
@@ -31,34 +28,34 @@ public class ComputeTest {
         builder.setShaderPath("add42");
         builder.parallelism(1);
 
-        ByteBuffer dataBuffer = MemoryUtil.memAlloc(4*4);
+        ByteBuffer dataBuffer = MemoryUtil.memAlloc(4);
         IntBuffer intBuffer = dataBuffer.asIntBuffer();
-        for (int i = 0; i < 4; i++) {
-            intBuffer.put(i, i+1);
-        }
+        intBuffer.put(0, 34);
 
-        StorageBuffer storageBuffer = new StorageBuffer(4*4);
+        StorageBuffer storageBuffer = new StorageBuffer(4);
         storageBuffer.copyToBuffer(dataBuffer);
 
         AlignedStruct.Builder descriptorBuilder = new AlignedStruct.Builder();
         descriptorBuilder.addUniformInfo("int", "vector");
         BufferDescriptor descriptor = descriptorBuilder.buildDescriptor(0, GraphicsPipeline.Builder.getStageFromString("compute"), VK_DESCRIPTOR_TYPE_STORAGE_BUFFER_DYNAMIC);
 
-
         builder.addDescriptor(descriptor);
         builder.compileShaders();
         ComputePipeline pipeline = builder.createComputePipeline();
 
         pipeline.updateBuffer(storageBuffer, 0);
-//        pipeline.bindDescriptorSets(computeManager.);
 
         computeManager.setPipeline(pipeline);
+
         long fence = computeManager.compute(1, 1, 1);
         computeManager.waitForFrame(fence);
-        IntBuffer result = storageBuffer.copyFromBuffer().asIntBuffer();
-        System.out.println(result.get(0));
-//        System.
 
-        System.out.println("sudsaces");
+        IntBuffer result = storageBuffer.copyFromBuffer().asIntBuffer();
+        int sum = result.get(0);
+
+        assert sum == 34 + 42;
+        System.out.println(sum);
+
+        System.out.println("succees");
     }
 }
